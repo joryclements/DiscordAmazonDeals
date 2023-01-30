652896a387be1459180e2db0476737a6e1263daa
Jory Clements <jory.clements@betterup.co>
2023-01-29 19:35:10 -0500

    WIP - Added DB - Working on getting ASIN more correct

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# DiscordAmazonDeals
+# Overview
+A simple bot for tracking Amazon prices. Currently, it only supports tracking prices for products in the Canada.
+This in the future will use the [Amazon Product Advertising API](https://affiliate-program.amazon.com/gp/advertising/api/detail/main.html) to get product information.
+
+# Commands
+### User Management
+* `/az add_user` - Add a user to the database
+* `/az remove_user` - Remove a user from the database
+* `/az list_users` - List all users in the database
+
+### Product Management
+* `/az add_product <url>` - Add a product to the database
+* `/az remove_product` - Remove a product from the database
+* `/az list_products` - List all products in the database
--- cogs/amazon_cog.py
+++ cogs/amazon_cog.py
@@ -1,11 +1,11 @@
-import json
+import datetime
 import discord
 import os
 import re as regex
 import bs4
 import requests
 from dotenv import load_dotenv
-from pycord.multicog import add_to_group
+import sqlite3
 
 load_dotenv(dotenv_path="settings.env")
 ALLOWED_GUILDS = [int(os.getenv("ALLOWED_GUILDS"))]
@@ -17,16 +17,20 @@ HEADERS = ({'User-Agent':
 ## User Functions
 ## These functions are used to create and manage user files
 async def new_user(ctx: discord.ApplicationContext):
-    try:
-        with open(f"data/{ctx.author.id}.json", "r") as f:
-            await ctx.respond("User already exists")
-    except FileNotFoundError:
-        with open(f"data/{ctx.author.id}.json", "w") as f:
-            json.dump({}, f)
-            await ctx.respond(f"Created user file {ctx.author.id}.json")
-    f.close()
+    # Add user to database
+    conn = sqlite3.connect(os.getenv("SQLITE_DATABASE"))
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (user_id) VALUES (?)", (ctx.author.id,))
+    conn.commit()
+    conn.close()
     return
-
+async def get_products(ctx: discord.ApplicationContext):
+    conn = sqlite3.connect(os.getenv("SQLITE_DATABASE"))
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM products WHERE user_id = ?", (ctx.author.id,))
+    products = cursor.fetchall()
+    conn.close()
+    return products
 
 ## Parsing Functions
 ## These functions are used to parse the HTML of the Amazon Canada website
@@ -51,20 +55,55 @@ def get_price(soup: bs4.BeautifulSoup):
     return price
 
 def get_asin(url: str):
-    pattern = regex.compile("http(s)*://www.amazon.c(a|om)/([\\w-]+/)?(dp|gp/product)/(\\w+/)?(\\w{10})")
-    m = pattern.match(url)
-    print(m.groups())
+    # Split the URL by the forward slash
+    url = url.split("/")
+    # Verify the URL is for Amazon.ca or Amazon.com
+    if url[2] != "www.amazon.ca" or url[2] != "www.amazon.com":
+        return "NAURL"
+    # Get the ASIN from the URL
+    for section in url:
+        if regex.match(r"/([a-zA-Z0-9]{10})(?:[/?]|$)", section):
+            return section
+    return "NASIN"
 
 
-    if m:
-        return m[4]
-    else:
-        return None
+## Database Functions
+def setup_db():
+    # Create a SQLite connection
+    conn = sqlite3.connect(os.getenv("SQLITE_DATABASE"))
+    cursor = conn.cursor()
+    # Create a table
+    cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY)
+    """)
+    # Create another table, with a foreign key to the users table
+    cursor.execute("""CREATE TABLE IF NOT EXISTS products (
+        asin TEXT PRIMARY KEY,
+        user_id INTEGER,
+        url TEXT,
+        title TEXT,
+        price TEXT,
+        date_added TEXT,
+        date_updated TEXT,
+        FOREIGN KEY (user_id) REFERENCES users (user_id))
+    """)
+    # Save (commit) the changes
+    conn.commit()
+    # Close the connection
+    conn.close()
 
 class Amazon(discord.Cog, name="az"):
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
+        setup_db()
+        # Create a MySQL connection
+        # self.conn = mysql.connector.connect(
+        #     host=os.getenv("MYSQL_HOST"),
+        #     user=os.getenv("MYSQL_USER"),
+        #     password=os.getenv("MYSQL_PASSWORD"),
+        #     database=os.getenv("MYSQL_DATABASE")
+        # )
 
     az = discord.SlashCommandGroup(name="az",
                                    description="AMAZON CANADA",
@@ -78,8 +117,17 @@ class Amazon(discord.Cog, name="az"):
         name="new_user",
         description="Create a new user file",
         guild_ids=ALLOWED_GUILDS)
+
     async def new_user(self, ctx: discord.ApplicationContext):
         await new_user(ctx)
+        return
+    @az.command(
+        name="view_products",
+        description="Get a list of saved products",
+        guild_ids=ALLOWED_GUILDS)
+    async def view_products(self, ctx: discord.ApplicationContext):
+        products = await get_products(ctx)
+        await ctx.respond(products)
         return
     @az.command(
         name="get_product",
@@ -94,6 +142,7 @@ class Amazon(discord.Cog, name="az"):
         price = get_price(soup)
         await ctx.respond(f"{title}\n{price}")
         return
+
     @az.command(
         name="get_asin",
         description="Get the ASIN of an Amazon product",
@@ -102,12 +151,16 @@ class Amazon(discord.Cog, name="az"):
     async def get_product(self, ctx: discord.ApplicationContext, url: str = "None"):
         await ctx.defer()
         asin = get_asin(url)
-        if asin is None:
-            await ctx.respond("Invalid URL")
+        if asin == "NAURL":
+            await ctx.respond("Invalid URL. Please use Amazon.ca or Amazon.com")
+            return
+        elif asin == "NASIN":
+            await ctx.respond("I couldn't find that products unique identifier")
             return
         else:
             await ctx.respond(f"ASIN: {asin}")
             return
+
     @az.command(
         name="save_product",
         description="Save the product to your tracked products",
@@ -124,13 +177,12 @@ class Amazon(discord.Cog, name="az"):
         else:
             title = get_title(soup)
             price = get_price(soup)
-            with open(f"data/{ctx.author.id}.json", "r") as f:
-                data = json.load(f)
-            f.close()
-            data[asin] = {"title": title, "price": price}
-            with open(f"data/{ctx.author.id}.json", "w") as f:
-                json.dump(data, f)
-            f.close()
-            await ctx.respond(f"Saved {title} to your tracked products")
+            conn = sqlite3.connect(os.getenv("SQLITE_DATABASE"))
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO products (asin, user_id, url, title, price, date_added, date_updated) VALUES "
+                           "(?, ?, ?, ?, ?, ?, ?)", (asin, ctx.author.id, url, title, price, datetime.datetime.now().isoformat(),
+                                                     datetime.datetime.now().isoformat()))
+            conn.commit()
+            conn.close()
+            await ctx.respond(f"Product saved: {title}")
             return
-
